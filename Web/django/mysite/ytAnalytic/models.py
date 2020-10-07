d6811319f48a5cc066057aee2cd5078c907007c5
Kyle West <westkw@appstate.edu>
2020-10-07 12:25:37 -0400

    added a video model

--- Web/django/mysite/ytAnalytic/models.py
+++ Web/django/mysite/ytAnalytic/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=200)
+    vid_id = models.CharField(max_length=50)
+    thumbnail = models.CharField(max_length=200)
+    tags = models.CharField(max_length=500)
