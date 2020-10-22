7ad803ba1fdd1482fda45514a3126cc9a510d92e
westkw <shred925@gmail.com>
2020-10-22 09:38:12 -0400

    Worked on tag filter and worked on VIDEO SELECTION

--- Web/django/mysite/ytAnalytic/scripts/sort.py
+++ Web/django/mysite/ytAnalytic/scripts/sort.py
@@ -4,12 +4,24 @@ import ytAnalytic.scripts.search_api as api
 import time
 
 def sort_tag(requests, tag, vid_list):
+    tag = tag.strip()
     filtered = []
     for vid in vid_list:
         print(filtered)
         time.sleep(1)
         tag_list = api.statistics(requests, vid['id'])['tags']
-        print(tag_list)
+        tag_list = [tag.lower() for tag in tag_list]
         if tag in tag_list:
+            print(tag)
             filtered.append(vid['id'])
     return filtered
+
+def tag_list(requests, vid_list):
+    tags = []
+    for vid in vid_list:
+        vid_tags = api.statistics(requests, vid['id'])['tags']
+        for tag in vid_tags:
+            tag = tag.lower()
+            if tag not in tags:
+                tags.append(tag)
+    return tags
