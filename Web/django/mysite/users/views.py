eebb83ba4352bf4817f38fcbf92ddafbe17f66c0
westkw <shred925@gmail.com>
2020-11-05 11:10:17 -0500

    added login and logout system, and added profile

--- Web/django/mysite/users/views.py
+++ Web/django/mysite/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def register(request):
@@ -10,10 +11,14 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, 'Account created!')
-            return redirect('ytAnalytic-home')
+            return redirect('login')
     else:
         form = UserCreationForm()
     context = {
         'form' : form
     }
     return render(request, 'users/register.html', context)
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
