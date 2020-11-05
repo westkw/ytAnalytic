0f52d7908b364c7a82f33e8db89a806931170d2f
westkw <shred925@gmail.com>
2020-11-05 10:21:39 -0500

    Finished registration form

--- Web/django/mysite/users/views.py
+++ Web/django/mysite/users/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, 'Account created!')
+            return redirect('ytAnalytic-home')
+    else:
+        form = UserCreationForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'users/register.html', context)
